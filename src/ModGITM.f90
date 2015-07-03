1b9d94d68421f1b2443a9d16328eccb98b5fa703
Aaron Ridley <ridley@umich.edu>
2015-07-02 22:34:49 -0400

    Added these files to compile and post process.

---
+++ src/ModGITM.f90
@@ -0,0 +1,305 @@
+!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+
+module ModGITM
+
+  use ModSizeGitm
+  use ModPlanet
+
+  implicit none
+
+  real :: dt = 0.0
+
+  integer :: iCommGITM, iProc, nProcs
+
+  real, allocatable :: dLonDist_GB(:,:,:,:)
+  real, allocatable :: InvDLonDist_GB(:,:,:,:)
+  real, allocatable :: dLonDist_FB(:,:,:,:)
+  real, allocatable :: InvDLonDist_FB(:,:,:,:)
+  real, allocatable :: dLatDist_GB(:,:,:,:)
+  real, allocatable :: InvDLatDist_GB(:,:,:,:)
+  real, allocatable :: dLatDist_FB(:,:,:,:)
+  real, allocatable :: InvDLatDist_FB(:,:,:,:)
+  real, allocatable :: Altitude_GB(:,:,:,:)
+  real, allocatable :: dAlt_GB(:,:,:,:)
+  real, allocatable :: RadialDistance_GB(:,:,:,:)
+  real, allocatable :: InvRadialDistance_GB(:,:,:,:)
+  real, allocatable :: Gravity_GB(:,:,:,:)
+  real, allocatable :: CellVolume(:,:,:,:)
+
+  ! RCMR
+  real :: f107_est, f107a_est, f107_msis, f107a_msis
+  real :: PhotoElectronHeatingEfficiency_est
+  integer :: Sat_Loc
+
+  ! Topography
+  real, allocatable :: dAltDLon_CB(:,:,:,:)
+  real, allocatable :: dAltDLat_CB(:,:,:,:)
+
+  real, dimension(-1:nLons+2, nBlocksMax) :: Longitude
+  real, dimension(-1:nLats+2, nBlocksMax) :: Latitude, TanLatitude, CosLatitude
+
+  real, dimension(nLons, nBlocksMax) :: GradLonM_CB, GradLon0_CB, GradLonP_CB
+  real, dimension(nLats, nBlocksMax) :: GradLatM_CB, GradLat0_CB, GradLatP_CB
+  real, dimension(nLons,nLats,nBlocksMax) :: Altzero
+
+  real, allocatable :: Rho(:,:,:,:)
+  real, allocatable :: Temperature(:,:,:,:)
+  real, allocatable :: InvScaleHeight(:,:,:,:)
+  real, allocatable :: Pressure(:,:,:,:)
+  real, allocatable :: NDensity(:,:,:,:)
+  real, allocatable :: eTemperature(:,:,:,:)
+  real, allocatable :: ITemperature(:,:,:,:)
+  real, allocatable :: IPressure(:,:,:,:)
+  real, allocatable :: ePressure(:,:,:,:)
+
+real, allocatable :: SpeciesDensity(:,:,:,:,:)
+real, allocatable :: SpeciesDensityOld(:,:,:,:,:)
+  
+  real, allocatable :: LogRhoS(:,:,:,:,:)
+  real, allocatable :: LogNS(:,:,:,:,:)
+  real, allocatable :: VerticalVelocity(:,:,:,:,:)
+
+  real, allocatable :: NDensityS(:,:,:,:,:)
+
+  real, allocatable :: IDensityS(:,:,:,:,:)
+  real, allocatable :: IRIDensity(:,:,:,:,:)
+
+  real :: VTEC(-1:nLons+2,-1:nLats+2,nBlocksMax)
+
+  real, allocatable :: Gamma(:,:,:,:)
+
+  real, allocatable :: KappaTemp(:,:,:,:)
+  real, allocatable :: Ke(:,:,:,:)
+  real, allocatable :: dKe(:,:,:,:)
+
+  real, dimension(nLons, nLats, nBlocksMax) :: &
+       SurfaceAlbedo, SurfaceTemp,SubsurfaceTemp, tinertia, &
+       dSubsurfaceTemp, dSurfaceTemp
+
+  real, allocatable :: cp(:,:,:,:)
+  real :: ViscCoef(0:nLons+1,0:nLats+1, 0:nAlts+1)
+
+  real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2) :: &
+       MeanIonMass, MeanMajorMass
+
+  real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2) :: &
+       e_gyro, i_gyro
+
+  real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2, 3) :: Collisions
+  real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nIonsAdvect, nSpecies) :: &
+       IonCollisions
+
+  real, allocatable :: B0(:,:,:,:,:)
+  real, allocatable :: MLatitude(:,:,:,:)
+  real :: MLT(-1:nLons+2, -1:nLats+2, -1:nAlts+2)
+  real, allocatable :: MLongitude(:,:,:,:)
+  real, allocatable :: DipAngle(:,:,:,:)
+  real, allocatable :: DecAngle(:,:,:,:)
+
+  real, allocatable :: b0_d1(:,:,:,:,:)
+  real, allocatable :: b0_d2(:,:,:,:,:)
+  real, allocatable :: b0_d3(:,:,:,:,:)
+  real, allocatable :: b0_e1(:,:,:,:,:)
+  real, allocatable :: b0_e2(:,:,:,:,:)
+  real, allocatable :: b0_e3(:,:,:,:,:)
+  real, allocatable :: b0_cD(:,:,:,:)
+  real, allocatable :: b0_Be3(:,:,:,:)
+
+  real, allocatable :: cMax_GDB(:,:,:,:,:)
+
+  real, dimension(1:nLons, 1:nLats, 1:nAlts, 3) :: &
+       IonDrag, Viscosity
+
+ ! AGB: Added pressure gradient
+  real, allocatable :: PressureGradient(:,:,:,:,:)
+
+  real, dimension(1:nLons, 1:nLats, 1:nAlts, nSpecies) :: &
+       VerticalIonDrag
+
+  real, allocatable :: Potential(:,:,:,:)
+
+  real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2, 3) :: &
+       ExB, EField
+
+  real, dimension(-1:nLons+2, -1:nLats+2) :: &
+       ElectronEnergyFlux, ElectronAverageEnergy, &
+       ElectronEnergyFluxMono, ElectronNumberFluxMono, &
+       ElectronEnergyFluxWave, ElectronNumberFluxWave
+
+  real, allocatable :: Velocity(:,:,:,:,:)
+  real, allocatable :: IVelocity(:,:,:,:,:)
+
+  logical            :: isFirstGlow = .True.  
+  logical            :: isInitialGlow 
+
+  real, allocatable :: Emissions(:,:,:,:,:)
+
+  real, allocatable :: vEmissionRate(:,:,:,:,:)
+  
+  real, allocatable :: PhotoElectronDensity(:,:,:,:,:)
+  real, allocatable :: PhotoElectronRate(:,:,:,:,:)
+  real, allocatable :: PhotoEFluxU(:,:,:,:,:)
+  real, allocatable :: PhotoEFluxD(:,:,:,:,:)
+  
+  real, allocatable :: PhotoEFluxTotal(:,:,:,:,:)
+  
+  real, dimension(nPhotoBins)                 :: PhotoEBins
+  real, dimension(-1:nLons+2, -1:nLats+2, -1:nAlts+2) :: TempUnit
+
+  real :: LocalTime(-1:nLons+2)
+
+  real :: SubsolarLatitude, SubsolarLongitude 
+  real :: MagneticPoleColat, MagneticPoleLon
+  real :: HemisphericPowerNorth, HemisphericPowerSouth
+
+  integer, parameter :: iEast_ = 1, iNorth_ = 2, iUp_ = 3, iMag_ = 4
+  integer, parameter :: iVIN_ = 1, iVEN_ = 2, iVEI_ = 3
+
+contains
+  !=========================================================================
+  subroutine init_mod_gitm
+
+
+    if(allocated(dLonDist_GB)) RETURN
+    allocate(dLonDist_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(InvDLonDist_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(dLonDist_FB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(InvDLonDist_FB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(dLatDist_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(InvDLatDist_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(dLatDist_FB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(InvDLatDist_FB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(Altitude_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(dAlt_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(RadialDistance_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(InvRadialDistance_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(Gravity_GB(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(CellVolume(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(dAltDLon_CB(nLons,nLats,nAlts,nBlocks))
+    allocate(dAltDLat_CB(nLons,nLats,nAlts,nBlocks))
+    allocate(Rho(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(Temperature(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(InvScaleHeight(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(Pressure(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(NDensity(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(eTemperature(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(ITemperature(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(IPressure(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(ePressure(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(SpeciesDensity(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nSpeciesAll, nBlocks))
+    allocate(SpeciesDensityOld(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nSpeciesAll, nBlocks))
+    allocate(LogRhoS(-1:nLons+2, -1:nLats+2, -1:nAlts+2, &
+       nSpecies, nBlocksMax))
+    allocate(LogNS(-1:nLons+2, -1:nLats+2, -1:nAlts+2, &
+       nSpecies, nBlocksMax))
+    allocate(VerticalVelocity(-1:nLons+2, -1:nLats+2, -1:nAlts+2, &
+       nSpecies, nBlocksMax))
+    allocate(NDensityS(-1:nLons+2, -1:nLats+2, -1:nAlts+2, &
+       nSpeciesTotal, nBlocksMax))
+    allocate(IDensityS(-1:nLons+2, -1:nLats+2, -1:nAlts+2, &
+       nIons, nBlocksMax))
+    allocate(IRIDensity(-1:nLons+2, -1:nLats+2, -1:nAlts+2, &
+       nIons, nBlocksMax))
+    allocate(Gamma(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(KappaTemp(nLons, nLats, 0:nAlts+1, nBlocks))
+    allocate(Ke(nLons, nLats, 0:nAlts+1, nBlocks))
+    allocate(dKe(nLons, nLats, 0:nAlts+1, nBlocks))
+    allocate(cp(nLons,nLats,0:nAlts+1,nBlocks))
+    allocate(B0(-1:nLons+2,-1:nLats+2,-1:nAlts+2,4,nBlocks))
+    allocate(MLatitude(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(MLongitude(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(DipAngle(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(DecAngle(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(b0_d1(-1:nLons+2,-1:nLats+2,-1:nAlts+2,3,nBlocks))
+    allocate(b0_d2(-1:nLons+2,-1:nLats+2,-1:nAlts+2,3,nBlocks))
+    allocate(b0_d3(-1:nLons+2,-1:nLats+2,-1:nAlts+2,3,nBlocks))
+    allocate(b0_e1(-1:nLons+2,-1:nLats+2,-1:nAlts+2,3,nBlocks))
+    allocate(b0_e2(-1:nLons+2,-1:nLats+2,-1:nAlts+2,3,nBlocks))
+    allocate(b0_e3(-1:nLons+2,-1:nLats+2,-1:nAlts+2,3,nBlocks))
+    allocate(b0_cD(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(b0_Be3(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nBlocks))
+    allocate(cMax_GDB(0:nLons+1,0:nLats+1,0:nAlts+1,3,nBlocks))
+    allocate(PressureGradient(1:nLons, 1:nLats, 1:nAlts, 3, nBlocks))
+    allocate(Potential(-1:nLons+2, -1:nLats+2, -1:nAlts+2, nBlocks))
+    allocate(Velocity(-1:nLons+2, -1:nLats+2, -1:nAlts+2, 3, nBlocks))
+    allocate(IVelocity(-1:nLons+2, -1:nLats+2, -1:nAlts+2, 3, nBlocks))
+    allocate(Emissions(nLons,nLats,nAlts,nEmissions,nBlocks))
+    allocate(vEmissionRate(nLons,nLats,nAlts,nEmissionWavelengths,nBlocks))
+    allocate(PhotoElectronDensity(nLons,nLats,nAlts,nPhotoBins,nBlocks))
+    allocate(PhotoElectronRate(nLons,nLats,nAlts,nPhotoBins,nBlocks))
+    allocate(PhotoEFluxU(nLons,nLats,nAlts,nPhotoBins,nBlocks))
+    allocate(PhotoEFluxD(nLons,nLats,nAlts,nPhotoBins,nBlocks))
+    allocate(PhotoEFluxTotal(nLons,nLats,nAlts,nBlocks,2))
+  end subroutine init_mod_gitm
+  !=========================================================================
+  subroutine clean_mod_gitm
+
+
+    if(.not.allocated(dLonDist_GB)) RETURN
+    deallocate(dLonDist_GB)
+    deallocate(InvDLonDist_GB)
+    deallocate(dLonDist_FB)
+    deallocate(InvDLonDist_FB)
+    deallocate(dLatDist_GB)
+    deallocate(InvDLatDist_GB)
+    deallocate(dLatDist_FB)
+    deallocate(InvDLatDist_FB)
+    deallocate(Altitude_GB)
+    deallocate(dAlt_GB)
+    deallocate(RadialDistance_GB)
+    deallocate(InvRadialDistance_GB)
+    deallocate(Gravity_GB)
+    deallocate(CellVolume)
+    deallocate(dAltDLon_CB)
+    deallocate(dAltDLat_CB)
+    deallocate(Rho)
+    deallocate(Temperature)
+    deallocate(InvScaleHeight)
+    deallocate(Pressure)
+    deallocate(NDensity)
+    deallocate(eTemperature)
+    deallocate(ITemperature)
+    deallocate(IPressure)
+    deallocate(ePressure)
+    deallocate(SpeciesDensity)
+    deallocate(SpeciesDensityOld)
+    deallocate(LogRhoS)
+    deallocate(LogNS)
+    deallocate(VerticalVelocity)
+    deallocate(NDensityS)
+    deallocate(IDensityS)
+    deallocate(IRIDensity)
+    deallocate(Gamma)
+    deallocate(KappaTemp)
+    deallocate(Ke)
+    deallocate(dKe)
+    deallocate(cp)
+    deallocate(B0)
+    deallocate(MLatitude)
+    deallocate(MLongitude)
+    deallocate(DipAngle)
+    deallocate(DecAngle)
+    deallocate(b0_d1)
+    deallocate(b0_d2)
+    deallocate(b0_d3)
+    deallocate(b0_e1)
+    deallocate(b0_e2)
+    deallocate(b0_e3)
+    deallocate(b0_cD)
+    deallocate(b0_Be3)
+    deallocate(cMax_GDB)
+    deallocate(PressureGradient)
+    deallocate(Potential)
+    deallocate(Velocity)
+    deallocate(IVelocity)
+    deallocate(Emissions)
+    deallocate(vEmissionRate)
+    deallocate(PhotoElectronDensity)
+    deallocate(PhotoElectronRate)
+    deallocate(PhotoEFluxU)
+    deallocate(PhotoEFluxD)
+    deallocate(PhotoEFluxTotal)
+  end subroutine clean_mod_gitm
+  !=========================================================================
+end module ModGITM
---
+++ src/ModSources.f90
@@ -0,0 +1,143 @@
+!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+
+module ModSources
+
+  use ModSizeGitm
+  use ModPlanet, only: nSpecies,nSpeciesTotal,nIons
+
+  !\
+  ! Sources for neutral temperature
+  !/
+
+  real, dimension(nLons, nLats, nAlts) :: &
+       Conduction, NOCooling, OCooling, ElectronHeating, &
+       AuroralHeating, JouleHeating, IonPrecipHeating, &
+       EddyCond,EddyCondAdia,MoleConduction
+
+  real, allocatable :: EuvHeating(:,:,:,:)
+  real, allocatable :: eEuvHeating(:,:,:,:)
+  real, allocatable :: PhotoElectronHeating(:,:,:,:)
+  real, allocatable :: RadCooling(:,:,:,:)
+  real, allocatable :: RadCoolingRate(:,:,:,:)
+  real, allocatable :: RadCoolingErgs(:,:,:,:)
+  real, allocatable :: EuvHeatingErgs(:,:,:,:)
+  real, allocatable :: LowAtmosRadRate(:,:,:,:)
+  real, allocatable :: UserHeatingRate(:,:,:,:)
+
+  real, dimension(nLons,nLats,nAlts,3) :: GWAccel = 0.0
+
+  !\
+  ! Reactions used in chemistry output
+  ! i.e. in2p_e -->  n2+ + e
+  !      ino_n  -->  no + n
+  !/
+
+  
+  integer, parameter :: nReactions = 26
+ real :: ChemicalHeatingSpecies(nLons, nLats, nAlts,nReactions)
+  real :: ChemicalHeatingS(nReactions)
+  real :: NeutralSourcesTotal(nAlts, nSpeciesTotal)
+  real :: NeutralLossesTotal(nAlts, nSpeciesTotal)
+  real, allocatable :: ISourcesTotal(:,:,:,:,:)
+  real, allocatable :: ILossesTotal(:,:,:,:,:)
+
+  integer, parameter ::    in2p_e = 1
+  integer, parameter ::    io2p_e = 2
+  integer, parameter ::    in2p_o = 3
+  integer, parameter ::    inop_e = 4
+  integer, parameter ::    inp_o2 = 5
+  integer, parameter ::    ino_n  = 6
+  integer, parameter ::    iop_o2 = 7
+  integer, parameter ::    in_o2  = 8
+  integer, parameter ::    io2p_n = 9
+  integer, parameter ::    io2p_no= 10
+  integer, parameter ::    io2p_n2= 11
+  integer, parameter ::    in2p_o2= 12
+  integer, parameter ::    inp_o  = 13
+  integer, parameter ::    iop_n2 = 14
+  integer, parameter ::    io1d_n2 = 15
+  integer, parameter ::    io1d_o2 = 16
+  integer, parameter ::    io1d_o  = 17
+  integer, parameter ::    io1d_e  = 18
+  integer, parameter ::    in2d_o2 = 19
+  integer, parameter ::    iop2d_e = 20
+  integer, parameter ::    in2d_o = 21
+  integer, parameter ::    in2d_e = 22
+  integer, parameter ::    iop2d_n2 =23
+  integer, parameter ::    iop2p_e = 24
+  integer, parameter ::    iop2p_o = 25
+  integer, parameter ::    iop2p_n2 = 26
+
+
+
+
+  !\
+  ! Stuff for auroral energy deposition and ionization
+  !/
+
+  real, dimension(:), allocatable :: &
+       ED_grid, ED_Energies, ED_Flux, ED_Ion, ED_Heating
+  integer :: ED_N_Energies, ED_N_Alts
+  real, dimension(nAlts) :: ED_Interpolation_Weight
+  integer, dimension(nAlts) :: ED_Interpolation_Index
+
+  real, allocatable :: AuroralIonRateS(:,:,:,:,:)
+  real, allocatable :: AuroralHeatingRate(:,:,:,:)
+  real, allocatable :: IonPrecipIonRateS(:,:,:,:,:)
+  real, allocatable :: IonPrecipHeatingRate(:,:,:,:)
+  real :: ChemicalHeatingRate(nLons, nLats, nAlts)
+
+  real :: HorizontalTempSource(nLons, nLats, nAlts)
+
+  real :: Diffusion(nLons, nLats, nAlts, nSpecies)
+  real :: NeutralFriction(nLons, nLats, nAlts, nSpecies)
+  real :: IonNeutralFriction(nLons, nLats, nAlts, nSpecies)
+
+  real, allocatable :: KappaEddyDiffusion(:,:,:,:)
+
+contains
+  !=========================================================================
+  subroutine init_mod_sources
+
+
+    if(allocated(EuvHeating)) RETURN
+    allocate(EuvHeating(nLons, nLats, nAlts,nBlocks))
+    allocate(eEuvHeating(nLons, nLats, nAlts,nBlocks))
+    allocate(PhotoElectronHeating(nLons, nLats, nAlts,nBlocks))
+    allocate(RadCooling(nLons, nLats, nAlts,nBlocks))
+    allocate(RadCoolingRate(nLons, nLats, nAlts,nBlocks))
+    allocate(RadCoolingErgs(nLons, nLats, nAlts,nBlocks))
+    allocate(EuvHeatingErgs(nLons, nLats, nAlts,nBlocks))
+    allocate(LowAtmosRadRate(nLons, nLats, nAlts,nBlocks))
+    allocate(UserHeatingRate(nLons, nLats, nAlts,nBlocks))
+    allocate(ISourcesTotal(nLons,nLats,nAlts,nIons-1,nBlocks))
+    allocate(ILossesTotal(nLons,nLats,nAlts,nIons-1,nBlocks))
+    allocate(AuroralIonRateS(nLons,nLats,nAlts,nSpecies,nBlocks))
+    allocate(AuroralHeatingRate(nLons,nLats,nAlts,nBlocks))
+    allocate(IonPrecipIonRateS(nLons,nLats,nAlts,nSpecies,nBlocks))
+    allocate(IonPrecipHeatingRate(nLons,nLats,nAlts,nBlocks))
+    allocate(KappaEddyDiffusion(nLons,nLats,-1:nAlts+2,nBlocks))
+  end subroutine init_mod_sources
+  !=========================================================================
+  subroutine clean_mod_sources
+    if(.not.allocated(EuvHeating)) RETURN
+    deallocate(EuvHeating)
+    deallocate(eEuvHeating)
+    deallocate(PhotoElectronHeating)
+    deallocate(RadCooling)
+    deallocate(RadCoolingRate)
+    deallocate(RadCoolingErgs)
+    deallocate(EuvHeatingErgs)
+    deallocate(LowAtmosRadRate)
+    deallocate(UserHeatingRate)
+    deallocate(ISourcesTotal)
+    deallocate(ILossesTotal)
+    deallocate(AuroralIonRateS)
+    deallocate(AuroralHeatingRate)
+    deallocate(IonPrecipIonRateS)
+    deallocate(IonPrecipHeatingRate)
+    deallocate(KappaEddyDiffusion)
+  end subroutine clean_mod_sources
+  !=========================================================================
+end module ModSources
