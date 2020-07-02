aec90a1d7b23f4fcc87f4fc86c7c0b69696df754
Aaron Ridley <ridley@umich.edu>
2020-07-02 12:02:11 -0400

    Removed all of the files.

--- util/CRASH/src/ModCaltz.f90
+++
@@ -1,5 +0,0 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-!  For more information, see http://csem.engin.umich.edu/tools/swmf
-module CRASH_ModCaltz
-
-end module CRASH_ModCaltz
