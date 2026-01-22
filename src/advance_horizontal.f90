45fdbf30ef718b6533b2e9384dc672571f30a25b
Aaron Ridley <ridley@umich.edu>
2026-01-22 14:27:13 -0500

    putting some limits on the densities

--- src/advance_horizontal.f90
+++ src/advance_horizontal.f90
@@ -362,8 +362,13 @@ subroutine advance_horizontal(iBlock)
               do iSpecies = 1, nSpecies
                  if (NewNum_CV(iLon, iLat, iSpecies) < 0.0) then
                     write(*,*) "Species : ", iSpecies, iLon, iLat, iBlock
-                    stop
-                    NewNum_CV(iLon, iLat, iSpecies) = 1.0
+                    !stop
+              if (iSpecies <= nSpecies) then
+		NewNum_CV(iLon, iLat, iSpecies) = MinNeutralDensityAdvect
+              else
+                NewNum_CV(iLon, iLat, iSpecies) = MinNeutralDensity
+              endif
+
                     IsFound=.true.
                  endif
               enddo
